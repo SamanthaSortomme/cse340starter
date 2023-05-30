d1b5c9c6314879228c8e7c60d3bef42bddaa3f2f
SamanthaSortomme <sor23001@byui.edu>
2023-05-30 08:50:05 -0700

    folders missing

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
