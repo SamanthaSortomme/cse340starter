a9265c412bb3cde80d0daa74e3d9780d3beb5556
SamanthaSortomme <sor23001@byui.edu>
2023-05-30 17:19:43 -0700

    lots of typing

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-/* ******************************************
- * This server.js file is the primary file of the
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
+
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -13,25 +7,19 @@ const app = express()
 
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-/* ***********************
- * Routes
- *************************/
+app.set("layout", "./layouts/layout")
 app.use(require("./routes/static"))
 // Index route
-app.get("/", function (req, res) { res.render("index", {title: "Home"})
-})
+// app.get("/", function (req, res) { res.render("index", {title: "Home"})
+// })
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+app.get('/', (req, res) => {
+  const year = new Date().getFullYear();
+  res.render('index', { title: 'Home', year });
+});
+
 const port = process.env.PORT
 const host = process.env.HOST
-
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
