317400444ed08fb6cab43dcdba7fbda6331cb1ec
SamanthaSortomme <sor23001@byui.edu>
2023-06-13 09:16:06 -0700

    account and register pages work, no nav

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -9,9 +9,16 @@ async function buildLogin(req, res, next) {
     flash: req.flash(),
   });
 }
-
+async function buildRegistration(req, res, next) {
+  let nav = await utilities.getNav();
+  res.render('account/register', {
+    title: 'Registration',
+    nav,
+  });
+}
 module.exports = {
   buildLogin,
+  buildRegistration,
   getAccount: (req, res, next) => {
     res.render('account/login', { title: 'Login', flash: req.flash() });
   },
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,19 +5,7 @@ const accountController = require('../controllers/accountController');
 
 router.get('/login', utilities.handleErrors(accountController.buildLogin));
 router.get('/register', accountController.buildRegistration);
-// async function buildLogin(req, res, next) {
-//   let nav = await utilities.getNav();
-//   res.render('account/login', {
-//     title: 'Login',
-//     nav,
-//   });
-// }
 
-// router.get('/', accountController.getAccount);
-
-
-// router.get('/login', buildLogin);
-// router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 router.get('/', accountController.getAccount);
 
