2ad012e92a1a6f8c194ffd8c014f9121154f05d8
SamanthaSortomme <sor23001@byui.edu>
2023-06-13 17:41:31 -0700

    says errors doesn't exist again in login.ejs

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,7 @@
 const utilities = require('../utilities');
 const Account = require('../models/account-model');
+const bcrypt = require("bcryptjs")
+
 
 
 // Deliver login view
@@ -20,11 +22,25 @@ async function registerAccount(req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await Account.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -44,8 +60,6 @@ async function registerAccount(req, res) {
     })
   }
 }
-
-
 
 async function buildRegister(req, res, next) {
   let nav = await utilities.getNav()
